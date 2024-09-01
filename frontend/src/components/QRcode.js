4f94fed48a79849b55410bb94fd5a7ae9ae7a27e
vardhman dewasi <145224153+vardhman-dewasi@users.noreply.github.com>
2024-09-01 22:56:50 +0530

    Update QRcode.js

--- frontend/src/components/QRcode.js
+++ frontend/src/components/QRcode.js
@@ -3,7 +3,7 @@ import QRCode from 'qrcode.react';
 const QRCodeComponent = ({shortenedUrl, urlValue}) => {
   return (
     <div>
-      <h2 className='m-4'>QR Code for: {shortenedUrl}</h2>
+      <h2 className='m-4'>QR Code for: shortenedUrl</h2>
       <QRCode value={urlValue} />
       
     </div>
